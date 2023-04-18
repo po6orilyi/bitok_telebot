53f630ac4edea68734bab94716e4729bdd47e781
po6orilyi <53261082+po6orilyi@users.noreply.github.com>
2023-04-18 16:36:01 +0300

    Update config.py

--- config.py
+++ config.py
@@ -1,7 +1,7 @@
 import os
 
-BOT_TOKEN = '6226006527:AAHj4YmD6x8yiLOxJclzCbZFUfn_htxNh8A'
-GROUP_ID = -932963184
+BOT_TOKEN = ''
+GROUP_ID = ''
 
 api_username = os.getenv('API_NAME')
 api_password = os.getenv('API_PASS')
@@ -10,4 +10,4 @@ request_timeout_seconds = 10
 callback_delay_seconds = 10
 
 webhook_port = 8443
-webhook_url = f'195.14.122.147:{webhook_port}/webtransaction'
+webhook_url = f'1.1.1.1:{webhook_port}/webtransaction'
